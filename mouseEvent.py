3ffe4e541d9894f1922c08b70b9a886018c3c389
TutoBienEtre <4l3x.l4f0n@gmail.com>
2023-11-20 12:04:37 +0100

    key selection, screenshot turn off, introducing logs

--- mouseEvent.py
+++ mouseEvent.py
@@ -2,23 +2,47 @@ import sys
 import requests
 import time
 import asyncio
-from pynput import mouse
-# input_value = sys.argv[1]
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
 
+import json
+# input_value = sys.argv[1]
+is_mouse_event: bool = False
+
+path = 'preferences.json'
+with open(path, 'r') as settings:
+    settings = json.load(settings)
+    if "Mouse" in settings['macroName']:
+        # print("mouse key detected:" + settings['macroName'])
+        is_mouse_event = True
+    else:
+        # print("keyboard key detected:" + settings['macroName'])
+        pass
 
 
 
 def on_click(x, y, button, pressed):
-    if not pressed:
-        print(f"released {button}", flush=True )
-    if button == button.right:
-        return False
+    if is_mouse_event == True:
+        if not pressed:
+            # print(button)
+            print(f"macro triggered", flush=True )
+
+def on_release(key):
+    msg = format(key.char)
+    # print(msg, settings['macroName'])
+    if msg == settings['macroName']:
+        print("macro triggered", flush=True)
+    if msg == "\x03":
+        exit()
+        
+    
+keyboard_listener = KeyboardListener(on_release=on_release)
+mouse_listener = MouseListener(on_click=on_click)
+
+keyboard_listener.start()
+mouse_listener.start()
+keyboard_listener.join()
+mouse_listener.join()
 
 
-
-# Collect events until released
-with mouse.Listener(
-        on_click=on_click,
-        ) as listener:
-    listener.join()
-
+    
--- screenshot.py
+++ screenshot.py
@@ -6,27 +6,31 @@ import PIL
 from datetime import datetime
 from PIL import Image
 
-
 x,y = pyautogui.size()
-print(f"width={x}\theight={y}")
-path = 'screenshots\\'
-window = pyautogui.getWindowsWithTitle("Flash Browser")
-if len(window) != 1:
-    print("[ERR] Could not recognize FlashBrowser")
-else:
-    window = window[0]
-    window.activate()
-    print(window.isActive)
-    left, top = window.topleft
-    right, bottom = window.bottomright
-    box = (left+10,  top, right-10, bottom)
-    print(box)
-    img_name = str(datetime.now().strftime("%d_%m_%Y-%H_%M_%S"))
-    print(img_name)
-    imageRef = path + img_name + ".png"
-    print(imageRef)
-    p = pyautogui.screenshot(imageRef)
-    im = Image.open(imageRef)
-    im = im.crop(box)
-    im.save(imageRef)
-
+if not os.path.exists("saved\\screenshots\\"): 
+    os.makedirs("saved\\screenshots\\") 
+path = 'saved\\screenshots\\'
+log_path = 'saved\\'
+with open(os.path.join(log_path, "logs.txt"), 'r+') as logs:
+    previous_logs = logs.read()
+    
+    window = pyautogui.getWindowsWithTitle("Flash Browser")
+    if len(window) != 1:
+        print("[ERR] Could not recognize FlashBrowser")
+    else:
+        window = window[0]
+        # print(window.isActive)
+        if window.isActive:
+            left, top = window.topleft
+            right, bottom = window.bottomright
+            box = (left+10,  top, right-10, bottom)
+            img_name = str(datetime.now().strftime("%d_%m_%Y-%H_%M_%S"))
+            imageRef = path + img_name + ".png"
+            p = pyautogui.screenshot(imageRef)
+            im = Image.open(imageRef)
+            im = im.crop(box)
+            im.save(imageRef)
+            print('screenshot saved')
+            log = f"[{str(datetime.now().strftime("%H:%M:%S"))}] screenshot saved\n"
+            logs.write(log)
+            
